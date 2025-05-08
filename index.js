3a33cb41c14f3346d04f55364fbbf5e7ed35c5ab
Vitor Pereira <vitorpereira041019@gmail.com>
2025-05-08 16:41:41 -0300

    Add example environment configuration and update .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.DB_HOST;
 const usersRouter = require('./routes/users');
 
 app.get('/', (req, res) => {
